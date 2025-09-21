0933df7a5b612db435726a9b97b930dac5af7906
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2025-09-21 18:35:46 -0500

    add maperar

--- studio/mapear_struct_mapearlos_a_datos_de_red.c
+++ studio/mapear_struct_mapearlos_a_datos_de_red.c
@@ -1,5 +1,13 @@
 
 #include <stdio.h>
+#include <string.h>
+
 int main(){
+	int a = 10;
+	int b = 0; 
+	printf("antes b: %d \n",b);
+	memcpy(&b,&a,sizeof(a));
+	printf("despues b: %d \n",b);
+
 	return 0;
 }
